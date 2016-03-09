94b0fabb0932adf69e587668f627a7827e366ad9
Paul Irish <paul.irish@gmail.com>
2016-03-08 17:11:04 -0800

    tweak

--- libs/header_clean/header_clean.pl
+++ libs/header_clean/header_clean.pl
@@ -33,7 +33,7 @@ for (my $i = 0; $i <= $#input; $i++) {
 
 		# Find the second file on the next line: +++ b/README.md
 		my $next = $input[++$i];
-		$next    =~ /^$ansi_sequence_regex\++ (\w\/)?(.+?)(\e|$)/;
+		$next    =~ /^$ansi_sequence_regex\++\s(\w\/)?(.+?)\e|$/;
 		if ($1) {
 			print $1; # Print out whatever color we're using
 		}
