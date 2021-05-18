e526f6b7abaac9e174b2956a16255c425a063c22
Scott Baker <scott.baker@directlink.coop>
2021-05-18 14:21:23 -0700

    Don't pull in upstream DiffHighlight
    We've diverged a little from upstream, so pulling it in directly
    will break things

--- update-deps.sh
+++ update-deps.sh
@@ -2,14 +2,3 @@
 
 # initalize the bats components
 git submodule sync && git submodule update --init
-
-
-DIFFHIGHLIGHT_RAW_URL_BASE="https://raw.githubusercontent.com/git/git/master/contrib/diff-highlight"
-DIFFHIGHLIGHT_FILES=( "DiffHighlight.pm" "README" )
-
-for file in "${DIFFHIGHLIGHT_FILES[@]}";
-do
-  url="$DIFFHIGHLIGHT_RAW_URL_BASE/$file"
-  echo "$url"
-  curl -#Lo "lib/$file" "$url"
-done
