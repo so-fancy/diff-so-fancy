0b39dfb145b9d17ef6f4d63f7f5ade7929e6a4b1
Scott Baker <scott@perturb.org>
2023-06-09 10:32:02 -0700

    Update `report-bug.sh` for #461

--- report-bug.sh
+++ report-bug.sh
@@ -1,60 +1,66 @@
 #!/bin/bash
 
 clipboard() {
-  local copy_cmd
-  if [ -n "$PBCOPY_SERVER" ]; then
-    local body="" # buffer
-    body=$(cat)
-    # while IFS= read -r buffer; do
-    #   body="$body$buffer\n";
-    # done
-    curl "$PBCOPY_SERVER" --data-urlencode body="$body" >/dev/null 2>&1
-    return $?
-  fi
-  if type putclip >/dev/null 2>&1; then
-    copy_cmd="putclip"
-  elif [ -e /dev/clipboard ];then
-    cat > /dev/clipboard
-    return 0
-  elif type clip >/dev/null 2>&1; then
-    if [[ $LANG = UTF-8 ]]; then
-      copy_cmd="iconv -f utf-8 -t shift_jis | clip"
-    else
-      copy_cmd=clip
-    fi
-    # copy_cmd=clip
-  elif command -v pbcopy >/dev/null 2>&1; then
-    copy_cmd="pbcopy"
-  elif command -v xclip >/dev/null 2>&1; then
-    # copy_cmd="xclip -i -selection clipboard"
-    copy_cmd="xclip"
-  elif command -v xsel >/dev/null 2>&1 ; then
-    local copy_cmd="xsel -b"
-  fi
-  if [ -n "$copy_cmd" ] ;then
-    eval "$copy_cmd"
-  else
-    echo "clipboard is unavailable" 1>&2
-  fi
+	local copy_cmd
+
+	if [ -n "$PBCOPY_SERVER" ]; then
+		local body="" # buffer
+		body=$(cat)
+		# while IFS= read -r buffer; do
+		#   body="$body$buffer\n";
+		# done
+		curl "$PBCOPY_SERVER" --data-urlencode body="$body" >/dev/null 2>&1
+		return $?
+	fi
+
+	if type putclip >/dev/null 2>&1; then
+		copy_cmd="putclip"
+	elif [ -e /dev/clipboard ];then
+		cat > /dev/clipboard
+		return 0
+	elif type clip >/dev/null 2>&1; then
+		if [[ $LANG = UTF-8 ]]; then
+			copy_cmd="iconv -f utf-8 -t shift_jis | clip"
+		else
+			copy_cmd=clip
+		fi
+		# copy_cmd=clip
+	elif command -v pbcopy >/dev/null 2>&1; then
+		copy_cmd="pbcopy"
+	elif command -v xclip >/dev/null 2>&1; then
+		# copy_cmd="xclip -i -selection clipboard"
+		copy_cmd="xclip"
+	elif command -v xsel >/dev/null 2>&1 ; then
+		local copy_cmd="xsel -b"
+	fi
+
+	if [ -n "$copy_cmd" ] ;then
+		eval "$copy_cmd"
+	else
+		echo "clipboard is unavailable" 1>&2
+	fi
 }
 
-file=${2:-diff.txt}
+if [ $# -eq 0 ]; then
+	echo "Usage: $0 'git diff HEAD..HEAD^'"
+	exit 7
+fi
+
+file=${2:-dsf-bug-report.txt}
 
 {
-  echo "$1"
-  eval "$1"
-  echo ""
-  echo ""
-  echo ""
+	echo "$1"
+	eval "$1"
+	echo ""
+	echo ""
+	echo ""
 
-  echo "$1 --color"
-  eval "$1 --color"
+	echo "$1 --color"
+	eval "$1 --color"
 
-  echo "git config pager.diff"
-  eval "git config pager.diff"
+	echo "git config --list | grep pager"
+	eval "git config --list | grep pager"
+} | base64 > "$file"
 
-  echo "git config pager.show"
-  eval "git config pager.show"
-} > "$file"
-
-base64 < "$file" | clipboard
+echo "Wrote file: $file"
+echo "Please open a new issue on Github and attach it"
--- reporting-bugs.md
+++ reporting-bugs.md
@@ -10,10 +10,6 @@ You can use [report-bug.sh](./report-bug.sh) we provide
 
 ```sh
 ./report-bug.sh 'git diff HEAD..HEAD^'
-
-# or
-
-curl https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/report-bug.sh | bash -s 'git diff HEAD..HEAD^' 'diff.txt'
 ```
 
-Grab the output file and attach to the GitHub issue you create. A base64 version is also copied to your clipboard so you can paste to the issue.
+Attach the output file to the GitHub issue you create.
